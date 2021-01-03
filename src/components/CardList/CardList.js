56f923c08a83d0874592beb8bfce662c7df250bd
Ahmet <scienceworker34@gmail.com>
2021-01-03 12:26:20 +0300

    add cardList style

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,17 +1,17 @@
 import Card from "../Card/Card";
-
+import {StyledCardList} from "./CardList.style";
 
 export const CardList = ({movieList}) => {
     
 
     return (
-        <div>
+        <StyledCardList>
            {movieList.map( (movie, index) => (
            <Card 
            key={index}
            moviePoster={movie.poster_path}
            title={movie.title}
            />))} 
-        </div>
+        </StyledCardList>
     )
 }
---
+++ src/components/CardList/CardList.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledCardList = styled.div`
+display:flex;
+gap: 1rem;
+flex-wrap: wrap;
+justify-content: center;
+`
