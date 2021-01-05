4d5f5a7769d16b7ae740004685a14aff60e7e10c
Ahmet <scienceworker34@gmail.com>
2021-01-05 23:39:03 +0300

    add moviedetails api call part-2

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {StyledMovieImage} from "./components/Card/Card.style";
 import axios from "axios";
 
+const movieDetailBaseUrl = "https://api.themoviedb.org/3/movie/"
+const apiKey = "a1aa13ecb541106f47234372288eb64b";
+const baseImageUrl = "https://image.tmdb.org/t/p/w500";
+
 
 export default function MovieDetails () {
+    
+    
+    
+
+    useEffect(()=>{
+        axios.get(
+            movieDetailBaseUrl+id, {params: {
+            api_key: apiKey,
+            }}
+        )
+        .then(res=>setMovieDetails(res?.data))
+        .catch()
+        .finally()
+    },[id])
 
     return (
         <div>
-            MovieDetails
+            <StyledMovieImage/>
         </div>
     )
 }
