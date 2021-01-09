8c428062cf480d070b5432f4cb0b2bb5e47b5fc0
Ahmet <scienceworker34@gmail.com>
2021-01-09 13:21:35 +0300

    Add searchbox

--- src/Movie.js
+++ src/Movie.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import "./App.css";
 import axios from "axios";
 import { CardList } from "./components/CardList/CardList";
@@ -6,9 +6,11 @@ import SearchBox from "./components/SearchBox/index";
 
 const baseUrl = "https://api.themoviedb.org/3/search/movie";
 const apiKey = "a1aa13ecb541106f47234372288eb64b";
+export const MovieContext = createContext();
 
 function Movie() {
   const [movieList, setMovieList] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState("war")
 
   useEffect(() => {
     axios
@@ -16,20 +18,22 @@ function Movie() {
         params: {
           api_key: apiKey,
           page: 1,
-          query: "war",
+          query: searchKeyword,
         },
       })
       .then((res) => setMovieList(res?.data?.results))
       .catch()
       .finally();
-  }, []);
+  }, [searchKeyword]);
 
   console.log(movieList);
 
   return (
     <div className="App">
+      <MovieContext.Provider value={{setSearchKeyword}}>
       <SearchBox/>
       <CardList movieList={movieList} />
+      </MovieContext.Provider>
     </div>
   );
 }
--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {useRef, useContext} from "react";  
+import { MovieContext } from '../../Movie';
 import {StyledSearchButton, StyledSearchInput, StyledSearchBox} from "./SearchBox.style";
 
 
 const SearchBox = () => {
+    const inputRef = useRef();
+    const {setSearchKeyword} = useContext(MovieContext);
 
     return (
         <StyledSearchBox>
-            <StyledSearchInput></StyledSearchInput>
-            <StyledSearchButton>Search</StyledSearchButton>
+            <StyledSearchInput ref={inputRef} isColored={false}/>
+            <StyledSearchButton onClick={() => setSearchKeyword(inputRef?.current?.value)}>Search</StyledSearchButton>
         </StyledSearchBox>
     )
 }
